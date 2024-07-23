3bfa35e730ed14add324dfa76689cdd3c068517c
Esduardo <eduartrob@gmail.com>
2024-07-22 21:09:49 -0600

    udpate-full

--- src/domain/users.ts
+++ src/domain/users.ts
@@ -1,5 +1,7 @@
 import WebSocket from "ws";
 export interface ConnectedUser {
+    readyState: number;
+    send(arg0: string): unknown;
     ws: WebSocket;
     username: string;
 }
--- src/infraestructure/ws/webSocket.ts
+++ src/infraestructure/ws/webSocket.ts
@@ -20,11 +20,17 @@ const startServer = () => {
       const token = authHeader.split(' ')[1];
       if(token){
         try {
-          const decoded = await verifyToken(token);
-          const userId = decoded.userId;
-          const username = decoded.username;
-          connectedUsers[userId] = { ws, username };
+          const decoded: any = await verifyToken(token);
+          const userId = decoded.idServer;
+          const username = decoded.name;
+
+          // Aquí estamos asumiendo que ws tiene las propiedades y métodos necesarios.
+          connectedUsers[userId] = { ws, username, readyState: ws.readyState, send: ws.send.bind(ws) };
+
           console.log(`Usuario conectado: ${username} (ID: ${userId})`);
+
+          // Enviar mensaje de conexión
+          ws.send(JSON.stringify({ type: 'connect', message: 'Conectado al servidor WS' }));
         } catch (error) {
           ws.close(1008, 'Invalid token');
         }
@@ -39,10 +45,10 @@ const startServer = () => {
       if (parsedMessage.type === 'auth'){
         const token = parsedMessage.token;
         const decoded = await verifyToken(token);
-        console.log("el id del us", decoded.userId)
         const userId = decoded.userId;
         const username = decoded.username;
-        connectedUsers[userId] = { ws, username };
+        connectedUsers[userId] = { ws, username, readyState: ws.readyState, send: ws.send.bind(ws) };
+
         const checkTokenInterval = setInterval(() => {
           const currentTime = Date.now() / 1000;
           if (decoded.exp && currentTime > decoded.exp) {
@@ -63,6 +69,48 @@ const startServer = () => {
           console.log(`\nCliente ${username} conectado.\n`);
         }
       }
+
+
+      function getUserIdFromWebSocket(ws: WebSocket): string | null {
+        for (const userId in connectedUsers) {
+          if (connectedUsers[userId].ws === ws) {
+            return userId;
+          }
+        }
+        return null;
+      }
+
+      switch (parsedMessage.event) {
+        //EVENTOS ENTRE SERVIDORES
+        
+        case "dbChange":
+          const targetUserId = parsedMessage.data.userId;
+          for (const [userId, userSocket] of Object.entries(connectedUsers)) {
+            if (userId === targetUserId) {
+              console.log('Sending message to:', userId);
+              userSocket.ws.send(JSON.stringify({event: 'updateListBovinos', data:parsedMessage.data}))
+            }
+          }
+        break;
+
+
+        //EVENTOS ENTRE SERVIDOR WS Y CLIENTE
+        case "getListBovinos":
+          const userId = getUserIdFromWebSocket(ws);
+          if (userId) {
+            const userInfo = connectedUsers[userId];
+            if (userInfo) {
+              const { username } = userInfo;
+              console.log(`Acción "getListBovinos" ejecutada por usuario: ${username} (ID: ${userId})`);
+              ws.send(JSON.stringify({ event: "getListBovinos"}));
+            } else {
+              console.log(`Usuario con ID ${userId} no encontrado en la lista de usuarios conectados.`);
+            }
+          } else {
+            console.log(`No se encontró el usuario asociado con el WebSocket.`);
+          }
+          break;
+      }
       
     })
     ws.on('close', async () =>{
@@ -80,4 +128,6 @@ const startServer = () => {
     console.log(`Server running on port: ${port}`);
   });
 };
+
+
 export { startServer };
